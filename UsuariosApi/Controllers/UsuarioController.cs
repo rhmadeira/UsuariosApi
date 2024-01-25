cb25704991b7a820fd58addb44141cee1478b1f0
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-01-25 14:37:34 -0300

    Adicionar arquivos de projeto.

---
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] Usuario usuario)
+        {
+            if (usuario == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+    }
+}
