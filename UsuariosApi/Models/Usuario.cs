c14231387b32520f22bc290d49114eeb023b5959
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-01-26 13:23:44 -0300

    feat: config connectionString

--- UsuariosApi/Models/Usuario.cs
+++ UsuariosApi/Models/Usuario.cs
@@ -1,6 +1,13 @@
-﻿namespace UsuariosApi.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UsuariosApi.Models;
+
+public class Usuario : IdentityUser
 {
-    public class Usuario
+    public DateTime DataNascimento { get; set; }
+
+    public Usuario() : base()
     {
+
     }
 }
