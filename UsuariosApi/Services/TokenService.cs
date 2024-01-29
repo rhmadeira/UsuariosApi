e69f652d2fbfcdb3d7957d17fc4ad5979e903c3e
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-01-29 13:55:12 -0300

    feat: generate token

---
+++ UsuariosApi/Services/TokenService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Services;
+
+public class TokenService
+{
+    public void GenerateToken(Usuario usuario)
+    {
+        Claim[] claims = new Claim[]
+        {
+            new Claim(ClaimTypes.Name, usuario.UserName),
+            new Claim(ClaimTypes.NameIdentifier, usuario.Id.ToString()),
+            new Claim(ClaimTypes.DateOfBirth, usuario.DataNascimento.ToString())
+        };
+
+        var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("aosihf293h8f98dfj"));
+
+        var signingCredentials = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(expires: DateTime.Now.AddMinutes(100), claims: claims, signingCredentials: signingCredentials);
+    
+    }
+}
